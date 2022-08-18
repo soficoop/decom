b66d1a6588fa5b91222dd31750561c5463900561
Aviran Katz <aviran.katz@gmail.com>
2022-08-18 17:29:56 +0300

    chore: add prettier

--- backend/dist/config/database.js
+++ backend/dist/config/database.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     connection: {
-        client: 'postgres',
+        client: "postgres",
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5433),
-            database: env('DATABASE_NAME', 'decom'),
-            user: env('DATABASE_USERNAME', 'postgres'),
-            password: env('DATABASE_PASSWORD', 'postgrespassword'),
-            ssl: env.bool('DATABASE_SSL', false),
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 5433),
+            database: env("DATABASE_NAME", "decom"),
+            user: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "postgrespassword"),
+            ssl: env.bool("DATABASE_SSL", false),
         },
     },
 });
