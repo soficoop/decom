263867f48c81f87d0ae7eab7a84c6ea052b7e7f0
Aviran Katz <aviran.katz@gmail.com>
2022-08-18 14:33:33 +0300

    initial commit

---
+++ backend/config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5433),
+      database: env('DATABASE_NAME', 'decom'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgrespassword'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
