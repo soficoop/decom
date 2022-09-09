008055775b4e5fd207d05035ec2b21da1fcfd9e7
adanhy <the.adan.hy@gmail.com>
2022-09-09 13:40:34 +0300

    fix suggestion page and new suggestion form

--- frontend/src/views/postNewSuggestion.ts
+++ frontend/src/views/postNewSuggestion.ts
@@ -1,11 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const ADD_SUGGESTION = gql`
-  mutation AddSuggestion($title: String!) {
-    addSuggestion(title: $title) {
-      id
-      title
-      description
+  mutation createSuggestion {
+    createSuggestion(data: { title: $title, content: $content }) {
+      data {
+        attributes {
+          title
+          content
+          image
+          score
+          community
+          upvotes
+          downvotes
+        }
+      }
     }
   }
 `;
