03800c6b6ff553c3d5a7b177d154546c5fe3eada
Aviran Katz <aviran.katz@gmail.com>
2022-10-30 10:44:58 +0200

    feat: add suggestionCount

--- backend/src/api/suggestion/content-types/suggestion/lifecycles.ts
+++ backend/src/api/suggestion/content-types/suggestion/lifecycles.ts
@@ -5,7 +5,27 @@ export default {
   },
   async beforeCreate(event) {
     event.params.data.score = calculateScore(new Date(), event.params.data.upvotes, event.params.data.downvotes);
+  },
+  async afterCreate(event) {
+    await updateSuggestionsInCommunity(event.result.community.id);
+  },
+  async afterDelete(event) {
+    const communities = await strapi.entityService.findMany('api::community.community', { _limit: -1 });
+    for (const community of communities) {
+      await updateSuggestionsInCommunity(community.id);
+    }
+  },
+  async afterUpdate(event) {
+    const communities = await strapi.entityService.findMany('api::community.community', { _limit: -1 });
+    for (const community of communities) {
+      await updateSuggestionsInCommunity(community.id);
+    }
   }
+};
+
+async function updateSuggestionsInCommunity(communityId) {
+  const suggestionsInCommunity = await strapi.entityService.findMany('api::suggestion.suggestion', { _limit: -1, filters: { community: communityId } });
+  await strapi.entityService.update('api::community.community', communityId, { data: { suggestionCount: suggestionsInCommunity.length } });
 }
 
 function calculateScore(createdAt: Date, upvotes: any, downvotes: any): any {
