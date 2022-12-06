14c52a8b8bd3b223750a2e26dc651e5c646a932e
Aviran Katz <aviran.katz@gmail.com>
2022-12-06 18:15:40 +0200

    refactor: api.ts

--- frontend/src/contexts/api.tsx
+++ frontend/src/contexts/api.tsx
@@ -22,9 +22,9 @@ function ApiProvider({ children }: { children: JSX.Element }) {
     new ApolloClient({
       uri: apiUrl,
       cache: new InMemoryCache(),
-      headers: password ? {
-        authorization: `Basic ${password}`
-      } : undefined
+      headers: password
+      ? { authorization: `Basic ${password}` }
+      : undefined
     })
   );
 
